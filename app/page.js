16f1d45d2fbec63142330486214e0c0fe9dcd189
Ishita Srivastav <ishita.sri001@gmail.com>
2026-02-18 10:14:06 +0530

    update

--- app/page.js
+++ app/page.js
@@ -21,6 +21,7 @@ export default function Home() {
       <WhyAIStruggles />
       <UnifiedRuntime/>
       <Features/>
+      
       {/* <CoreEnterpriseSystem /> */}
       <CoreEnterpriseSystemSticky/>
       <UseCases/>
