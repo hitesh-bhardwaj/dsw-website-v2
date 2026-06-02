57e7d7edff2b22a02475fd19a2a81c3dec1cbea7
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2026-06-02 12:34:26 +0530

    Update

--- components/Homepage/PricingTableWrapper.jsx
+++ components/Homepage/PricingTableWrapper.jsx
@@ -10,25 +10,5 @@ export default async function PricingTableWrapper({ searchParams }) {
     headersList.get("cf-ipcountry") ||
     "";
 
-  console.log("Country:", country);
-
-  return (
-    <>
-      <div
-        style={{
-          position: "fixed",
-          top: 20,
-          right: 20,
-          background: "red",
-          color: "white",
-          padding: "10px",
-          zIndex: 99999,
-        }}
-      >
-        Country: {country}
-      </div>
-
-      <PricingTable region={country === "IN" ? "IN" : "US"} />
-    </>
-  );
+return <PricingTable region={country === "IN" ? "IN" : "US"} />;
 }
